01772f3ef24f276d44305091f774602ae7c0e699
KevinRuvalcaba <38799934+KevinRuvalcaba@users.noreply.github.com>
2020-04-16 21:58:54 -0500

    Arreglos de la clase Xdd

---
+++ public/js/app.js
@@ -0,0 +1,28 @@
+function showToast(msg){
+    console.log('El imput est: ', msg);
+    $.toast({
+        text: msg,
+        position: 'top-right'
+    });
+}
+
+window.socket = null;
+function connectToSocketIo(){
+    let server = window.location.protocol + "//" + window.location.host;
+    window.socket = io.connect(server);
+    window.socket.on("toast", function(data){
+        showToast(data.message);
+    });
+}
+
+function messageToServer(msg) {
+    window.socket.emit('message-to-server', {message: msg});
+}
+
+
+
+$(function() {
+    //
+    connectToSocketIo();
+});
+
